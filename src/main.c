f7ff32a6dfcb758e756bbf8fb30e34c9717e8b84
Pietro Signorino <signorino9@gmail.com>
2024-05-11 19:54:49 +0200

    Added test main.c

--- src/main.c
+++ src/main.c
@@ -1,19 +1,18 @@
 #include "numeric.h"
 
 int main() {
-  numeric_t *a = create_numeric_(3.6, true);
-  numeric_t *b = create_numeric_(-2.312, true);
+  // true enables gradient storing
+  numeric_t *a = create_numeric_(-3.6, true);
+  numeric_t *b = create_numeric_(2.312, true);
   numeric_t *c = create_numeric_(2, true);
 
-  // (a - b) ^ c
-  numeric_t *loss =
-      numeric_sub(numeric_abs(numeric_div(numeric_sub(a, b), c)), a);
-  print_numeric(loss);
+  // abs(a - b) / c
+  numeric_t *loss = numeric_div(numeric_abs(numeric_sub(a, b)), c);
 
   // Compute gradients via Backpropagation
   backprop(loss);
 
-  // Let's print stored gradients
+  // Print variables with stored gradients
   print_numeric(a);
   print_numeric(b);
   print_numeric(c);
