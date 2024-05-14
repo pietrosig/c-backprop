968a3c9a6b626c96f0c428a85cf65ec40fec3cdf
Pietro Signorino <signorino9@gmail.com>
2024-05-14 22:52:47 +0200

    Code refactor - numeric_mat.c

--- src/numeric_math.c
+++ src/numeric_math.c
@@ -1,16 +1,22 @@
 #include "numeric.h"
 
-// Computes ---> op1 + op2
-numeric_t *numeric_add(numeric_t *op1, numeric_t *op2) {
+numeric_t *numeric_create_result(double result, numeric_t *op1, numeric_t *op2,
+                                 grad_calc_t grad_fn) {
 
-  numeric_t *res = create_numeric(op1->n + op2->n);
+  numeric_t *res = create_numeric(result);
 
   res->op1 = op1;
   res->op2 = op2;
 
-  res->grad_fn = &ADD_GRAD_CALC_;
+  res->grad_fn = grad_fn;
 
   return res;
+}
+
+// Computes ---> op1 + op2
+numeric_t *numeric_add(numeric_t *op1, numeric_t *op2) {
+  double res = op1->n + op2->n;
+  return numeric_create_result(res, op1, op2, &ADD_GRAD_CALC_);
 }
 
 // Computes ---> op1 - op2
@@ -24,15 +30,8 @@ numeric_t *numeric_sub(numeric_t *op1, numeric_t *op2) {
 
 // Computes ---> op1 * op2
 numeric_t *numeric_mul(numeric_t *op1, numeric_t *op2) {
-
-  numeric_t *res = create_numeric(op1->n * op2->n);
-
-  res->op1 = op1;
-  res->op2 = op2;
-
-  res->grad_fn = &MUL_GRAD_CALC_;
-
-  return res;
+  double res = op1->n * op2->n;
+  return numeric_create_result(res, op1, op2, &MUL_GRAD_CALC_);
 }
 
 // Computes ---> op1 / op2
@@ -46,96 +45,51 @@ numeric_t *numeric_div(numeric_t *op1, numeric_t *op2) {
 
 // Computes ---> op1 ^ op2
 numeric_t *numeric_pow(numeric_t *op1, numeric_t *op2) {
-
-  numeric_t *res = create_numeric(pow(op1->n, op2->n));
-
-  res->op1 = op1;
-  res->op2 = op2;
-
-  res->grad_fn = &POW_GRAD_CALC_;
-
-  return res;
+  double res = pow(op1->n, op2->n);
+  return numeric_create_result(res, op1, op2, &POW_GRAD_CALC_);
 }
 
 // Computes ---> 1 / op1
 // This can be computed as numeric_pow(op1, -1)
 // But directly computing 1 / x is more efficient than computing x ^ -1
 numeric_t *numeric_inv(numeric_t *op1) {
-
-  numeric_t *res = create_numeric(1 / op1->n);
-
-  res->op1 = op1;
-
-  res->grad_fn = &INV_GRAD_CALC_;
-
-  return res;
+  double res = 1 / op1->n;
+  return numeric_create_result(res, op1, NULL, &INV_GRAD_CALC_);
 }
 
 // Computes ---> e ^ op1
 // This can be computed as numeric_pow(e, op1)
 numeric_t *numeric_exp(numeric_t *op1) {
-
-  numeric_t *res = create_numeric(exp(op1->n));
-
-  res->op1 = op1;
-
-  res->grad_fn = &EXP_GRAD_CALC_;
-
-  return res;
+  double res = exp(op1->n);
+  return numeric_create_result(res, op1, NULL, &EXP_GRAD_CALC_);
 }
 
 // Computes ---> log(op1)
 numeric_t *numeric_log(numeric_t *op1) {
-
-  numeric_t *res = create_numeric(log(op1->n));
-
-  res->op1 = op1;
-
-  res->grad_fn = &LOG_GRAD_CALC_;
-
-  return res;
+  double res = log(op1->n);
+  return numeric_create_result(res, op1, NULL, &LOG_GRAD_CALC_);
 }
 
 // Computes ---> abs(op1)
 numeric_t *numeric_abs(numeric_t *op1) {
-  numeric_t *res = create_numeric(fabs(op1->n));
-
-  res->op1 = op1;
-
-  res->grad_fn = &ABS_GRAD_CALC_;
-
-  return res;
+  double res = fabs(op1->n);
+  return numeric_create_result(res, op1, NULL, &ABS_GRAD_CALC_);
 }
 
 // Computes ---> sin(op1)
 numeric_t *numeric_sin(numeric_t *op1) {
-  numeric_t *res = create_numeric(sin(op1->n));
-
-  res->op1 = op1;
-
-  res->grad_fn = &SIN_GRAD_CALC_;
-
-  return res;
+  double res = sin(op1->n);
+  return numeric_create_result(res, op1, NULL, &SIN_GRAD_CALC_);
 }
 
 // Computes ---> cos(op1)
 numeric_t *numeric_cos(numeric_t *op1) {
-  numeric_t *res = create_numeric(cos(op1->n));
-
-  res->op1 = op1;
-
-  res->grad_fn = &COS_GRAD_CALC_;
-
-  return res;
+  double res = cos(op1->n);
+  return numeric_create_result(res, op1, NULL, &COS_GRAD_CALC_);
 }
 
 // Computes ---> ReLu(op1)
 numeric_t *numeric_relu(numeric_t *op1) {
-  numeric_t *res = create_numeric(fmax(0, op1->n));
-
-  res->op1 = op1;
-
-  res->grad_fn = &RELU_GRAD_CALC_;
-
-  return res;
+  double res = fmax(0, op1->n);
+  return numeric_create_result(res, op1, NULL, &RELU_GRAD_CALC_);
 }
