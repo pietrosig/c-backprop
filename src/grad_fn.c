2621861a674a4b564e126771ec69256aa5bd2aad
Pietro Signorino <signorino9@gmail.com>
2024-05-16 16:01:29 +0200

    Added <errors.h> for proper error handling

--- src/grad_fn.c
+++ src/grad_fn.c
@@ -1,8 +1,8 @@
+#include "errors.h"
 #include "numeric.h"
 
 double ADD_GRAD_CALC_(numeric_t *output, numeric_t *input) {
-  if (output == NULL || input == NULL)
-    return 0;
+  GRAD_FN_INPUT_CHECK_(output, input);
 
   // Derivative w.r.t. op1 ---> 1
   // Derivative w.r.t. op2 ---> 1
@@ -10,8 +10,7 @@ double ADD_GRAD_CALC_(numeric_t *output, numeric_t *input) {
 }
 
 double MUL_GRAD_CALC_(numeric_t *output, numeric_t *input) {
-  if (output == NULL || input == NULL)
-    return 0;
+  GRAD_FN_INPUT_CHECK_(output, input);
 
   // Derivative w.r.t. op1 ---> (op1 * op2) / op1 = op2
   // Derivative w.r.t. op2 ---> (op1 * op2) / op2 = op1
@@ -19,72 +18,61 @@ double MUL_GRAD_CALC_(numeric_t *output, numeric_t *input) {
 }
 
 double POW_GRAD_CALC_(numeric_t *output, numeric_t *input) {
-  if (output == NULL || input == NULL)
-    return 0;
+  GRAD_FN_INPUT_CHECK_(output, input);
 
   // Derivative w.r.t. op1 --> (op1 ^ (op2 - 1)) * op2
   if (output->op1 == input)
     return (output->n / output->op1->n) * output->op2->n;
 
   // Derivative w.r.t. op2 --> (op1 ^ op2) * log(op1)
-  if (output->op2 == input)
-    return output->n * log(output->op1->n);
-
-  // TODO: ERROR
-  return 0;
+  // No check needed due to GRAD_FN_INPUT_CHECK_
+  return output->n * log(output->op1->n);
 }
 
 double INV_GRAD_CALC_(numeric_t *output, numeric_t *input) {
-  if (output == NULL || input == NULL)
-    return 0;
+  GRAD_FN_INPUT_CHECK_(output, input);
 
   // Derivative w.r.t. op1 --> -1 / (op1 ^ 2)
   return -(output->n * output->n);
 }
 
 double EXP_GRAD_CALC_(numeric_t *output, numeric_t *input) {
-  if (output == NULL || input == NULL)
-    return 0;
+  GRAD_FN_INPUT_CHECK_(output, input);
 
   // Derivative w.r.t. op1 --> e ^ op1
   return output->n;
 }
 
 double LOG_GRAD_CALC_(numeric_t *output, numeric_t *input) {
-  if (output == NULL || input == NULL)
-    return 0;
+  GRAD_FN_INPUT_CHECK_(output, input);
 
   // Derivative w.r.t. op1 --> 1 / op1
   return 1 / input->n;
 }
 
 double ABS_GRAD_CALC_(numeric_t *output, numeric_t *input) {
-  if (output == NULL || input == NULL)
-    return 0;
+  GRAD_FN_INPUT_CHECK_(output, input);
 
   // Derivative w.r.t. op1 --> abs(op1) / op1
   return output->n / input->n;
 }
 
 double SIN_GRAD_CALC_(numeric_t *output, numeric_t *input) {
-  if (output == NULL || input == NULL)
-    return 0;
+  GRAD_FN_INPUT_CHECK_(output, input);
 
   // Derivative w.r.t. op1 --> cos(op1)
   return cos(input->n);
 }
 
 double COS_GRAD_CALC_(numeric_t *output, numeric_t *input) {
-  if (output == NULL || input == NULL)
-    return 0;
+  GRAD_FN_INPUT_CHECK_(output, input);
 
   // Derivative w.r.t. op1 --> -sin(op1)
   return -sin(input->n);
 }
 
 double RELU_GRAD_CALC_(numeric_t *output, numeric_t *input) {
-  if (output == NULL || input == NULL)
-    return 0;
+  GRAD_FN_INPUT_CHECK_(output, input);
 
   // Derivative w.r.t. op1 --> 1 if (op1 > 0)
   //                           0 else
--- src/numeric_math.c
+++ src/numeric_math.c
@@ -1,12 +1,13 @@
+#include "errors.h"
 #include "numeric.h"
 
 // Creates a result numeric with right operands and grad_function
 numeric_t *numeric_create_result(double result, numeric_t *op1, numeric_t *op2,
                                  grad_calc_t grad_fn) {
-  // Error!!
-  // TODO: Error handling
-  if (op1 == NULL && op2 == NULL)
-    return NULL;
+
+  BOTH_NULL_POINTERS_CHECK_(op1, op2);
+
+  NULL_POINTER_CHECK_(grad_fn);
 
   numeric_t *res = create_numeric(result);
 
@@ -20,12 +21,18 @@ numeric_t *numeric_create_result(double result, numeric_t *op1, numeric_t *op2,
 
 // Computes ---> op1 + op2
 numeric_t *numeric_add(numeric_t *op1, numeric_t *op2) {
+  NULL_POINTER_CHECK_(op1);
+  NULL_POINTER_CHECK_(op2);
+
   double res = op1->n + op2->n;
   return numeric_create_result(res, op1, op2, &ADD_GRAD_CALC_);
 }
 
 // Computes ---> op1 - op2
 numeric_t *numeric_sub(numeric_t *op1, numeric_t *op2) {
+  NULL_POINTER_CHECK_(op1);
+  NULL_POINTER_CHECK_(op2);
+
   // partial = -op2
   numeric_t *partial = numeric_mul(op2, NUMERIC_NEG_ONE);
 
@@ -34,12 +41,17 @@ numeric_t *numeric_sub(numeric_t *op1, numeric_t *op2) {
 
 // Computes ---> op1 * op2
 numeric_t *numeric_mul(numeric_t *op1, numeric_t *op2) {
+  NULL_POINTER_CHECK_(op1);
+  NULL_POINTER_CHECK_(op2);
+
   double res = op1->n * op2->n;
   return numeric_create_result(res, op1, op2, &MUL_GRAD_CALC_);
 }
 
 // Computes ---> op1 / op2
 numeric_t *numeric_div(numeric_t *op1, numeric_t *op2) {
+  // No check because this is a composed function!
+
   // partial = 1 / op2
   numeric_t *partial = numeric_inv(op2);
 
@@ -48,6 +60,9 @@ numeric_t *numeric_div(numeric_t *op1, numeric_t *op2) {
 
 // Computes ---> op1 ^ op2
 numeric_t *numeric_pow(numeric_t *op1, numeric_t *op2) {
+  NULL_POINTER_CHECK_(op1);
+  NULL_POINTER_CHECK_(op2);
+
   double res = pow(op1->n, op2->n);
   return numeric_create_result(res, op1, op2, &POW_GRAD_CALC_);
 }
@@ -56,6 +71,8 @@ numeric_t *numeric_pow(numeric_t *op1, numeric_t *op2) {
 // This can be computed as numeric_pow(op1, -1)
 // But directly computing 1 / x is more efficient than computing x ^ -1
 numeric_t *numeric_inv(numeric_t *op1) {
+  NULL_POINTER_CHECK_(op1);
+
   double res = 1 / op1->n;
   return numeric_create_result(res, op1, NULL, &INV_GRAD_CALC_);
 }
@@ -63,36 +80,48 @@ numeric_t *numeric_inv(numeric_t *op1) {
 // Computes ---> e ^ op1
 // This can be computed as numeric_pow(e, op1)
 numeric_t *numeric_exp(numeric_t *op1) {
+  NULL_POINTER_CHECK_(op1);
+
   double res = exp(op1->n);
   return numeric_create_result(res, op1, NULL, &EXP_GRAD_CALC_);
 }
 
 // Computes ---> log(op1)
 numeric_t *numeric_log(numeric_t *op1) {
+  NULL_POINTER_CHECK_(op1);
+
   double res = log(op1->n);
   return numeric_create_result(res, op1, NULL, &LOG_GRAD_CALC_);
 }
 
 // Computes ---> abs(op1)
 numeric_t *numeric_abs(numeric_t *op1) {
+  NULL_POINTER_CHECK_(op1);
+
   double res = fabs(op1->n);
   return numeric_create_result(res, op1, NULL, &ABS_GRAD_CALC_);
 }
 
 // Computes ---> sin(op1)
 numeric_t *numeric_sin(numeric_t *op1) {
+  NULL_POINTER_CHECK_(op1);
+
   double res = sin(op1->n);
   return numeric_create_result(res, op1, NULL, &SIN_GRAD_CALC_);
 }
 
 // Computes ---> cos(op1)
 numeric_t *numeric_cos(numeric_t *op1) {
+  NULL_POINTER_CHECK_(op1);
+
   double res = cos(op1->n);
   return numeric_create_result(res, op1, NULL, &COS_GRAD_CALC_);
 }
 
 // Computes ---> ReLu(op1)
 numeric_t *numeric_relu(numeric_t *op1) {
+  NULL_POINTER_CHECK_(op1);
+
   double res = fmax(0, op1->n);
   return numeric_create_result(res, op1, NULL, &RELU_GRAD_CALC_);
 }
